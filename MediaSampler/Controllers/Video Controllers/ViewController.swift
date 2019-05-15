3531de4a54fc2e609582bdd4a66ad49d2851e58e
Marquis Dennis <marquis103@gmail.com>
2019-05-15 18:48:04 -0400

    initial commit and file organization

---
+++ MediaSampler/Controllers/Video Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MediaSampler
+//
+//  Created by Marquis Dennis on 5/15/19.
+//  Copyright © 2019 MDennis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
